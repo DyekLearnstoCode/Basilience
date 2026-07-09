87b915a7e4dea6efbd25c41dfa4c6cfd5418ba8b
Dyek <20230284@cstc.edu.ph>
2026-07-09 22:18:21 +0800

    change to linearlayout

--- app/src/main/java/com/example/basilience/MainActivity.java
+++ app/src/main/java/com/example/basilience/MainActivity.java
@@ -7,11 +7,23 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import android.util.Log;
 import com.example.basilience.AlertManager;
 
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCardView activeAlertBanner;
+    private TextView alertTitle;
+    private TextView alertMessage;
 
     private AlertManager alertManager;
 
@@ -20,8 +32,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        activeAlertBanner = findViewById(R.id.activeAlertBanner);
+        activeAlertBanner.setTranslationY(-300f);
+        alertTitle = findViewById(R.id.alertTitle);
+        alertMessage = findViewById(R.id.alertMessage);
+
         alertManager = new AlertManager(this);
         alertManager.startListening();
+        startAlertBannerListener();
 
         FirebaseDatabase.getInstance()
                 .getReference("test")
@@ -52,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                     bottomNav.getMenu().findItem(R.id.home).setChecked(true);
                 } else if (id == R.id.Notification) {
                     bottomNav.getMenu().findItem(R.id.Notification).setChecked(true);
-                } else if (id == R.id.settings || id == R.id.accountFragment || 
+                } else if (id == R.id.settings || id == R.id.accountFragment ||
                            id == R.id.aboutFragment || id == R.id.tosFragment) {
                     bottomNav.getMenu().findItem(R.id.settings).setChecked(true);
                 }
@@ -76,5 +94,151 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+    }
+
+    private void startAlertBannerListener() {
+
+        FirebaseDatabase.getInstance(
+                        "https://basilience-database-default-rtdb.asia-southeast1.firebasedatabase.app"
+                )
+                .getReference("device")
+                .addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+
+                        DataSnapshot alerts =
+                                snapshot.child("alerts");
+
+                        DataSnapshot status =
+                                snapshot.child("status");
+
+                        StringBuilder message =
+                                new StringBuilder();
+
+                        boolean hasAlert = false;
+
+                        if(Boolean.TRUE.equals(
+                                alerts.child("phOutOfRange")
+                                        .getValue(Boolean.class)))
+                        {
+                            hasAlert = true;
+
+                            message.append(
+                                    "🧪 pH Out Of Range\n"
+                            );
+
+                            if(Boolean.TRUE.equals(
+                                    status.child("phUp")
+                                            .getValue(Boolean.class)))
+                            {
+                                message.append(
+                                        "Action: Dosing pH Up\n\n"
+                                );
+                            }
+                            else if(Boolean.TRUE.equals(
+                                    status.child("phDown")
+                                            .getValue(Boolean.class)))
+                            {
+                                message.append(
+                                        "Action: Dosing pH Down\n\n"
+                                );
+                            }
+                        }
+
+                        if(Boolean.TRUE.equals(
+                                alerts.child("ecLow")
+                                        .getValue(Boolean.class)))
+                        {
+                            hasAlert = true;
+
+                            message.append(
+                                    "🌱 EC Low\n"
+                            );
+
+                            if(Boolean.TRUE.equals(
+                                    status.child("nutrients")
+                                            .getValue(Boolean.class)))
+                            {
+                                message.append(
+                                        "Action: Nutrient Pump Running\n\n"
+                                );
+                            }
+                            else
+                            {
+                                message.append(
+                                        "Action: Awaiting Correction\n\n"
+                                );
+                            }
+                        }
+
+                        if(Boolean.TRUE.equals(
+                                alerts.child("highTemperature")
+                                        .getValue(Boolean.class)))
+                        {
+                            hasAlert = true;
+
+                            message.append(
+                                    "🌡 High Temperature\n"
+                            );
+
+                            if(Boolean.TRUE.equals(
+                                    status.child("canopyFan")
+                                            .getValue(Boolean.class)))
+                            {
+                                message.append(
+                                        "Action: Cooling Fan Active\n\n"
+                                );
+                            }
+                        }
+
+                        if(Boolean.TRUE.equals(
+                                alerts.child("lowWater")
+                                        .getValue(Boolean.class)))
+                        {
+                            hasAlert = true;
+
+                            message.append(
+                                    "💧 Low Water Level\n"
+                            );
+
+                            message.append(
+                                    "Action: Refill Required\n\n"
+                            );
+                        }
+
+                        if(hasAlert)
+                        {
+                            if (activeAlertBanner.getVisibility() != View.VISIBLE) {
+
+                                activeAlertBanner.setVisibility(View.VISIBLE);
+
+                                activeAlertBanner.animate()
+                                        .translationY(0)
+                                        .setDuration(300)
+                                        .start();
+                            }
+
+                            alertMessage.setText(
+                                    message.toString()
+                            );
+                        }
+                        else
+                        {
+                            activeAlertBanner.animate()
+                                    .translationY(-300)
+                                    .setDuration(300)
+                                    .withEndAction(() ->
+                                            activeAlertBanner.setVisibility(View.GONE))
+                                    .start();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(
+                            DatabaseError error
+                    ) {
+                    }
+                });
     }
 }
